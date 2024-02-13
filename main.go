7ef9086adbf21709e8bae6fe1dfe8ed4171b0b86
DRublev <aringai09@gmail.com>
2024-02-13 21:51:42 +0300

    fix ticker

--- main.go
+++ main.go
@@ -78,11 +78,11 @@ func restoreState() {
 }
 
 func main() {
-	// if _, isDev := os.LookupEnv("DEV"); isDev {
-	if err := godotenv.Load(); err != nil {
-		log.Fatalln("Cannot load env!")
+	if _, isDev := os.LookupEnv("DEV"); isDev {
+		if err := godotenv.Load(); err != nil {
+			log.Fatalln("Cannot load env!")
+		}
 	}
-	// }
 
 	// defer db.persist(state)
 
